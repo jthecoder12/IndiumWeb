006913bc134885fca935b35d8d5464e165ca5f15
jthecoder12 <128553511+jthecoder12@users.noreply.github.com>
2023-07-02 10:33:45 +0530

    Add code for blue background

---
+++ webindium.js
@@ -0,0 +1,14 @@
+main();
+
+function main() {
+    const canvas = document.querySelector("gameCanvas");
+    const gl = canvas.getContext("webgl");
+
+    if(gl == null) {
+        alert("Initialization failed");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
